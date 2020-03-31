0e07812122b3144018807d892564216f5e1ee2fa
Joseph Kao <josephtkao@gmail.com>
2020-03-31 23:06:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board, index)
-  if (index >= 0 && index <= 8 && position_taken?(board,index))
+  if (index >= 0 && index <= 8 && !position_taken?(board,index))
     puts "true"
     return true
   else
